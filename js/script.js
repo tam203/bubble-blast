362ea2abb802acce99960c289b535e0716288933
Fintan Kearney <fintanak@gmail.com>
2016-03-30 18:47:35 -0300

    Content for click event

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
-var x = document.getElementsByClassName("vr-image-demo");
-var i;
-for (i = 0; i < x.length; i++) {
-    x[i].addEventListener('click', function () {
-        alert(1);
-        window.location.href = '/360image.html?urk=' + this.getAttribute("src");
-    });
-}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    var x = document.getElementsByClassName("vr-image-demo");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].addEventListener('click', function () {
+            window.location.href = this.getAttribute("content-url");
+        });
+    }
+});
