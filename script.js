c16f1ba52e0e5860b5fa9a9ab31c6264f4d84d63
Fintan Kearney <fintanak@gmail.com>
2016-03-30 13:20:57 -0300

    Favicon and others

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.querySelector('.vr-image-demo').addEventListener('click', function () {
-    window.location.href = '/360image.html?urk=' + this.getAttribute("src");
-});
+var x = document.getElementsByClassName("vr-image-demo");
+var i;
+for (i = 0; i < x.length; i++) {
+    x[i].addEventListener('click', function () {
+        alert(1);
+        window.location.href = '/360image.html?urk=' + this.getAttribute("src");
+    });
+}
